f1a7608a3b96ac17068aabbecda4d7f8258a5597
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-08 05:02:12 +0000

    [Add]user_idの追加、設定およびbooks/indexの表示部分の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     @book.save
     #セーブし終わったら、bookのshowページに推移させる。
     redirect_to book_path(@book.id)
@@ -24,9 +25,9 @@ class BooksController < ApplicationController
   end
 
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
