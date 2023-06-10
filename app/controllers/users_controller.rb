d958561273431cc31750385d19be3b3d9656cca7
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-10 12:00:08 +0000

    [Add]booksのshowページ作成途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = current_user
   end
 
   def show
     @user = current_user
+    @books = Book.all
   end
 
   def edit
