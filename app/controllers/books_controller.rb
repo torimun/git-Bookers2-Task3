2b531d5ae3a75111df6f3ff0c54d3b907d9ae55e
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-15 04:55:29 +0000

    [Add]books/editのバリデーション設定、editページでのupdateエラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,10 +7,13 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    #セーブし終わったら、bookのshowページに推移させる。
-    redirect_to book_path(@book.id)
+    if @book.save
+       flash[:notice] = "You have created book successfully."
+       #セーブし終わったら、bookのshowページに推移させる。
+       redirect_to book_path(@book.id)
+    else
+       render :new
+    end
   end
 
   def index
@@ -32,9 +35,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(book.id)
+    if @book.update(book_params)
+       flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
