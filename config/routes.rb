f5974e2149e043b30b2ff8393c80b5f5445934cb
ToriMun <132326048+torimun@users.noreply.github.com>
2023-06-17 12:39:16 +0000

    [Add]aboutページのリンク、非ログイン時に表示できるよう設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-  get  '/homes/about', to: 'homes#about', as: 'about'
+  get  '/home/about', to: 'homes#about', as: 'about'
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
 
